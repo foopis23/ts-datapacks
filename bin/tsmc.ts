afab9051d97a0d8c6f05609e05656cda63204aac
Eric Sexton <foopis23@gmail.com>
2024-09-09 14:33:23 -0400

    clean up

--- bin/tsmc.ts
+++ bin/tsmc.ts
@@ -2,58 +2,53 @@
 import { program } from "commander";
 import { TSDataPack } from "../lib/ts-datapack";
 
-(async () => {
-  program
-    .name("data-pack-lib")
-    .description("A library for creating data packs for Minecraft")
-    .version("0.0.1");
+program
+  .name("data-pack-lib")
+  .description("A library for creating data packs for Minecraft")
+  .version("0.0.1");
 
-  program
-    .command("init")
-    .description("Initialize a new data pack")
-    .argument("<projectName>", "Name of the project")
-    .action(async (ctx) => {
-      const projectName: string = ctx;
-      const newProjectDir = await TSDataPack.createMCDatapack(projectName);
+program
+  .command("init")
+  .description("Initialize a new data pack")
+  .argument("<projectName>", "Name of the project")
+  .action(async (ctx) => {
+    const projectName: string = ctx;
+    const newProjectDir = await TSDataPack.createMCDatapack(projectName);
 
-      console.log(
-        `Initialized new project in ${newProjectDir}.\n Run 'cd ${projectName}' and 'bun install' to get started.`
-      );
-    });
+    console.log(
+      `Initialized new project in ${newProjectDir}.\n Run 'cd ${projectName}' and 'bun install' to get started.`
+    );
+  });
 
-  program
-    .command("build")
-    .description("Generate and bundle the data pack")
-    .action(async () => {
-      await TSDataPack.fromDefaultConfig().then((tsDataPack) =>
-        tsDataPack.build()
-      );
-    });
+program
+  .command("build")
+  .description("Generate and bundle the data pack")
+  .action(async () => {
+    await TSDataPack.fromDefaultConfig().then((tsDataPack) =>
+      tsDataPack.build()
+    );
+  });
 
-  const debugSubCommands = program.command("debug");
+const debugSubCommands = program.command("debug");
 
-  debugSubCommands
-    .command("generate")
-    .description(
-      "Generate dynamic elements of the data pack. This is mostly for debugging."
-    )
-    .action(async () => {
-      await TSDataPack.fromDefaultConfig().then((tsDataPack) =>
-        tsDataPack.generateAll()
-      );
-    });
+debugSubCommands
+  .command("generate")
+  .description(
+    "Generate dynamic elements of the data pack. This is mostly for debugging."
+  )
+  .action(async () => {
+    await TSDataPack.fromDefaultConfig().then((tsDataPack) =>
+      tsDataPack.generateAll()
+    );
+  });
 
-  debugSubCommands
-    .command("bundle")
-    .description("Bundle the data pack")
-    .action(async () => {
-      await TSDataPack.fromDefaultConfig().then((tsDataPack) =>
-        tsDataPack.bundle()
-      );
-    });
+debugSubCommands
+  .command("bundle")
+  .description("Bundle the data pack")
+  .action(async () => {
+    await TSDataPack.fromDefaultConfig().then((tsDataPack) =>
+      tsDataPack.bundle()
+    );
+  });
 
-  program.parse(process.argv);
-})().catch((err) => {
-  console.error(err);
-  process.exit(1);
-});
+program.parse(process.argv);
